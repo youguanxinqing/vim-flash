5b670fb1892730d903b55edb77e449a83939e836
bzy-debug <bzy_sustech@foxmail.com>
2025-03-03 23:49:12 +0800

    highlight mvp

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,61 @@
 import * as vscode from "vscode";
 
+function search(
+  content: string,
+  target: string,
+  baseLine: number,
+): vscode.Range[] {
+  const ranges: vscode.Range[] = [];
+  let line = baseLine;
+  let column = 0;
+  for (let i = 0; i < content.length; i++) {
+    if (content[i] === "\n") {
+      line++;
+      column = 0;
+    } else {
+      column++;
+    }
+    if (content[i] === target) {
+      ranges.push(
+        new vscode.Range(
+          new vscode.Position(line, column - 1),
+          new vscode.Position(line, column),
+        ),
+      );
+    }
+  }
+  return ranges;
+}
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "flash" is now active!');
 
-  const disposable = vscode.commands.registerCommand("flash.helloWorld", () => {
-    vscode.window.showInformationMessage("Hello World from neovim flash!");
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand("flash.flash", (editor) => {
+      const dim = vscode.window.createTextEditorDecorationType({
+        color: "rgb(119, 119, 119)",
+      });
+      const highlight = vscode.window.createTextEditorDecorationType({
+        backgroundColor: "rgba(255, 255, 0, 0.5)",
+      });
+      editor.setDecorations(dim, editor.visibleRanges);
+      const d = vscode.commands.registerCommand(
+        "type",
+        ({ text }: { text: string }) => {
+          console.log(editor.visibleRanges);
+          const ranges = editor.visibleRanges.flatMap((range) =>
+            search(editor.document.getText(range), text, range.start.line),
+          );
+          editor.setDecorations(highlight, ranges);
+          setTimeout(() => {
+            d.dispose();
+            dim.dispose();
+            highlight.dispose();
+          }, 3000);
+        },
+      );
+    }),
+  );
 }
 
 export function deactivate() {}
